47cc9d86110b1149301ba3ffa1376dc256e2b65f
kryllyxofficial01 <kryllyxofficial@gmail.com>
2024-04-19 20:19:45 -0400

    /mcserver_config: check port, account for missing parameters

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import interactions, mcstatus, json, pathlib, dotenv, os
 from interactions import Client, SlashContext, Intents, OptionType, SlashCommandChoice
 from interactions.ext import prefixed_commands
 from interactions.ext.prefixed_commands import prefixed_command, PrefixedContext
-import socket
+import typing
 
 dotenv.load_dotenv()
 
@@ -15,7 +15,8 @@ if not os.path.exists(config_path):
 client = Client(
     token = os.getenv("TOKEN"),
     default_scope = 1167687057360027668,
-    intents = Intents.GUILDS | Intents.MESSAGE_CONTENT | Intents.GUILD_MESSAGES
+    intents = Intents.GUILDS | Intents.MESSAGE_CONTENT | Intents.GUILD_MESSAGES,
+    send_command_tracebacks = False # go go gadget stacktrace dump
 )
 prefixed_commands.setup(client, default_prefix="!")
 
@@ -97,10 +98,18 @@ async def mcserver_config(context: SlashContext, config_name: str, config_value:
         error = False
         if config_name == "port":
             try: config_value = int(config_value)
+
             except ValueError:
-                print("\033[1;33m/mcserver_config\033[0;0m: Prior call contained a \033[1;31mnon-integer port\033[0;0m")
+                print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call contained a \033[1;31mnon-integer port ({config_value})\033[0;0m")
                 await context.send("Port must be an integer")
                 error = True
+
+            else:
+               if config_value < 0 or config_value > 65535: # max port value
+                    print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call contained an \033[1;31minvalid port ({config_value})\033[0;0m")
+                    await context.send("Port must be greater than 0 and less than 65535")
+                    error = True
+
 
         if not error:
             configs[config_name] = config_value
@@ -148,37 +157,53 @@ async def playerlist_legacy(context: PrefixedContext):
         await context.reply("Connection timed out, likely due to an invalid server domain and/or port configuration")
 
 @prefixed_command(name="mcserver_config")
-async def mcserver_config_legacy(context: PrefixedContext, config_name: str, config_value: str):
+async def mcserver_config_legacy(context: PrefixedContext, config_name: typing.Optional[str], config_value = None):
     print(f"\033[1;33m!mcserver_config\033[0;0m: Called by \033[0;32m{context.author.username}\033[0;0m")
 
-    operator_role = context.guild.get_role(1167687375913234464) # hard coded role ID because I'm lazy, shut up
+    if config_name == None:
+        print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call \033[1;31mmissing configuration name\033[0;0m")
+        await context.reply("You must provide a configuration")
 
-    valid_configs = ["domain", "port"]
-
-    if context.author.has_role(operator_role):
-        with open(config_path, "r") as config_file:
-            configs = json.load(config_file)
-
-        if config_name in valid_configs:
-            error = False
-            if config_name == "port":
-                try: config_value = int(config_value)
-                except ValueError:
-                    print("\033[1;33m/mcserver_config\033[0;0m: Prior call contained a \033[1;31mnon-integer port\033[0;0m")
-                    await context.reply("Port must be an integer")
-                    error = True
-
-            if not error:
-                configs[config_name] = config_value
-
-                with open(config_path, "w") as config_file:
-                    json.dump(configs, config_file, indent=4)
-
-                print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call changed \033[0;32m{config_name.capitalize()}\033[0;0m to \033[0;32m{config_value}\033[0;0m")
-                await context.reply(f"Updated '{config_name.capitalize()}' to be '{config_value}'")
+    elif config_value == None:
+        print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call \033[1;31mmissing configuration value\033[0;0m")
+        await context.reply("You must provide a value to set the configuration to")
 
     else:
-        print("\033[1;33m/mcserver_config\033[0;0m: Prior call \033[1;31mnot permitted\033[0;0m")
-        await context.reply("You do not have permission to use that command.")
+        operator_role = context.guild.get_role(1167687375913234464) # hard coded role ID because I'm lazy, shut up
+
+        valid_configs = ["domain", "port"]
+
+        if context.author.has_role(operator_role):
+            with open(config_path, "r") as config_file:
+                configs = json.load(config_file)
+
+            if config_name in valid_configs:
+                error = False
+                if config_name == "port":
+                    try: config_value = int(config_value)
+
+                    except ValueError:
+                        print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call contained a \033[1;31mnon-integer port ({config_value})\033[0;0m")
+                        await context.reply("Port must be an integer")
+                        error = True
+
+                    else:
+                        if config_value < 0 or config_value > 65535: # min and max port value
+                            print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call contained an \033[1;31minvalid port ({config_value})\033[0;0m")
+                            await context.reply("Port must be greater than 0 and less than 65535")
+                            error = True
+
+                if not error:
+                    configs[config_name] = config_value
+
+                    with open(config_path, "w") as config_file:
+                        json.dump(configs, config_file, indent=4)
+
+                    print(f"\033[1;33m/mcserver_config\033[0;0m: Prior call changed \033[0;32m{config_name.capitalize()}\033[0;0m to \033[0;32m{config_value}\033[0;0m")
+                    await context.reply(f"Updated '{config_name.capitalize()}' to be '{config_value}'")
+
+        else:
+            print("\033[1;33m/mcserver_config\033[0;0m: Prior call \033[1;31mnot permitted\033[0;0m")
+            await context.reply("You do not have permission to use that command.")
 
 client.start()
