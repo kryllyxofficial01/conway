a8a3bf36d9c742273e2bda574b60621efb1f5ebf
kryllyxofficial01 <kryllyxofficial@gmail.com>
2024-04-23 20:09:43 -0400

    start on refactoring

--- src/main.py
+++ src/main.py
@@ -1,44 +1,40 @@
-import interactions, mcstatus, json, pathlib, dotenv, os
-from interactions import Client, SlashContext, Intents, OptionType, SlashCommandChoice
+import mcstatus, dotenv, os, typing, ipaddress, utils
+from interactions import slash_command, slash_option, Client, SlashContext, Intents, OptionType, SlashCommandChoice
 from interactions.ext import prefixed_commands
 from interactions.ext.prefixed_commands import prefixed_command, PrefixedContext
-import typing, ipaddress, utils
+from utils import CONFIG_PATH
 
 dotenv.load_dotenv()
 
-config_path = str(pathlib.Path(__file__).parent.absolute()) + "/configs.json"
-
-if not os.path.exists(config_path):
-    with open(config_path, 'w') as file:
-        json.dump({}, file, indent=4)
+if not os.path.exists(CONFIG_PATH):
+   utils.refresh_config({})
 
 client = Client(
     token = os.getenv("TOKEN"),
-    default_scope = 1167687057360027668,
     intents = Intents.GUILDS | Intents.MESSAGE_CONTENT | Intents.GUILD_MESSAGES,
     send_command_tracebacks = False # go go gadget stacktrace dump
 )
 prefixed_commands.setup(client, default_prefix="!")
 
-@interactions.slash_command(
+@slash_command(
     name = "ping",
     description = "Returns the latency of the bot"
 )
 async def ping(context: SlashContext):
-    print(f"{utils.get_current_time()} | \033[1;33m/ping\033[0;0m: Called by \033[0;32m{context.author.username}\033[0;0m")
+    print(utils.log_command_call("ping", context.author.username))
+
     await context.send(f"Ping: {round(context.bot.latency * 1000, 2)} ms")
 
-@interactions.slash_command(
+@slash_command(
     name = "playerlist",
     description = "Get the list of players currently online."
 )
 async def playerlist(context: SlashContext):
     await context.defer()
 
-    print(f"{utils.get_current_time()} | \033[1;33m/playerlist\033[0;0m: Called by \033[0;32m{context.author.username}\033[0;0m")
+    print(utils.log_command_call("playerlist", context.author.username))
 
-    with open(config_path, "r") as config_file:
-        configs = json.load(config_file)
+    configs = utils.load_config()
 
     domain = configs["domain"]
     port = configs["port"]
@@ -46,26 +42,23 @@ async def playerlist(context: SlashContext):
     try:
         mcserver = mcstatus.JavaServer(
             host = domain,
-            port = port,
+            port = port
         )
 
-        try:
-            output = ", ".join([player.name for player in mcserver.status().players.sample])
-        except TypeError:
-            output = "No players are online."
-
-        await context.send(output)
+        try: output = ", ".join([player.name for player in mcserver.status().players.sample])
+        except TypeError: output = "No players are online."
+        else: await context.send(output)
 
     except:
         print(f"{utils.get_current_time()} | \033[1;33m/playerlist\033[0;0m: Prior call \033[1;31mtimed out\033[0;0m")
 
         await context.send("Connection timed out, likely due to an invalid server domain and/or port configuration")
 
-@interactions.slash_command(
+@slash_command(
     name = "mcserver_config",
     description = "Change server info for use in the MC API."
 )
-@interactions.slash_option(
+@slash_option(
     name = "config_name",
     description = "Name of the config being changed.",
     required = True,
@@ -81,24 +74,24 @@ async def playerlist(context: SlashContext):
         )
     ]
 )
-@interactions.slash_option(
+@slash_option(
     name = "config_value",
     description = "Value the config is being changed to.",
     required = True,
     opt_type = OptionType.STRING
 )
 async def mcserver_config(context: SlashContext, config_name: str, config_value: str):
-    print(f"{utils.get_current_time()} | \033[1;33m/mcserver_config\033[0;0m: Called by \033[0;32m{context.author.username}\033[0;0m")
+    print(utils.log_command_call("mcserver_config", context.author.username))
 
     operator_role = context.guild.get_role(1167687375913234464) # hard coded role ID because I'm lazy, shut up
 
     if context.author.has_role(operator_role):
-        with open(config_path, "r") as config_file:
-            configs = json.load(config_file)
+        configs = utils.load_config()
 
         error = False
         if config_name == "domain":
             try: ipaddress.ip_address(config_value)
+
             except ValueError:
                 error = True
 
@@ -127,8 +120,7 @@ async def mcserver_config(context: SlashContext, config_name: str, config_value:
         if not error:
             configs[config_name] = config_value
 
-            with open(config_path, "w") as config_file:
-                json.dump(configs, config_file, indent=4)
+            utils.refresh_config(configs)
 
             print(f"{utils.get_current_time()} | \033[1;33m/mcserver_config\033[0;0m: Prior call changed \033[0;32m{config_name.capitalize()}\033[0;0m to \033[0;32m{config_value}\033[0;0m")
 
@@ -141,16 +133,15 @@ async def mcserver_config(context: SlashContext, config_name: str, config_value:
 
 @prefixed_command(name="ping")
 async def ping_legacy(context: PrefixedContext):
-    print(f"{utils.get_current_time()} | \033[1;33m!ping\033[0;0m: Called by \033[0;32m{context.author.username}\033[0;0m")
+    print(utils.log_command_call("ping", context.author.username, is_slash_command=False))
 
     await context.reply(f"Ping: {round(context.bot.latency * 1000, 2)} ms")
 
 @prefixed_command(name="playerlist")
 async def playerlist_legacy(context: PrefixedContext):
-    print(f"{utils.get_current_time()} | \033[1;33m!playerlist\033[0;0m: Called by \033[0;32m{context.author.username}\033[0;0m")
+    print(utils.log_command_call("playerlist", context.author.username, is_slash_command=False))
 
-    with open(config_path, "r") as config_file:
-        configs = json.load(config_file)
+    configs = utils.load_config()
 
     domain = configs["domain"]
     port = configs["port"]
@@ -161,21 +152,19 @@ async def playerlist_legacy(context: PrefixedContext):
             port = port,
         )
 
-        try:
-            output = ", ".join([player.name for player in mcserver.status().players.sample])
-        except TypeError:
-            output = "No players are online."
-
-        await context.reply(output)
-
     except:
         print(f"{utils.get_current_time()} | \033[1;33m/playerlist\033[0;0m: Prior call \033[1;31mtimed out\033[0;0m")
 
         await context.reply("Connection timed out, likely due to an invalid server domain and/or port configuration")
 
+    else:
+        try: output = ", ".join([player.name for player in mcserver.status().players.sample])
+        except TypeError: output = "No players are online."
+        else: await context.reply(output)
+
 @prefixed_command(name="mcserver_config")
 async def mcserver_config_legacy(context: PrefixedContext, config_name: typing.Optional[str], config_value = None):
-    print(f"{utils.get_current_time()} | \033[1;33m!mcserver_config\033[0;0m: Called by \033[0;32m{context.author.username}\033[0;0m")
+    print(utils.log_command_call("mcserver_config", context.author.username, is_slash_command=False))
 
     if config_name == None:
         print(f"{utils.get_current_time()} | \033[1;33m/mcserver_config\033[0;0m: Prior call \033[1;31mmissing configuration name\033[0;0m")
@@ -193,13 +182,13 @@ async def mcserver_config_legacy(context: PrefixedContext, config_name: typing.O
         valid_configs = ["domain", "port"]
 
         if context.author.has_role(operator_role):
-            with open(config_path, "r") as config_file:
-                configs = json.load(config_file)
+            configs = utils.load_config()
 
             if config_name in valid_configs:
                 error = False
                 if config_name == "domain":
                     try: ipaddress.ip_address(config_value)
+
                     except ValueError:
                         error = True
 
@@ -228,8 +217,7 @@ async def mcserver_config_legacy(context: PrefixedContext, config_name: typing.O
                 if not error:
                     configs[config_name] = config_value
 
-                    with open(config_path, "w") as config_file:
-                        json.dump(configs, config_file, indent=4)
+                    utils.refresh_config(configs)
 
                     print(f"{utils.get_current_time()} | \033[1;33m/mcserver_config\033[0;0m: Prior call changed \033[0;32m{config_name.capitalize()}\033[0;0m to \033[0;32m{config_value}\033[0;0m")
 
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,22 @@
+import json, pathlib
 from datetime import datetime
+
+CONFIG_PATH = str(pathlib.Path(__file__).parent.absolute()) + "/configs.json"
 
 def get_current_time() -> str:
     return datetime.now().strftime("%H:%M:%S.%f")
+
+def log_command_call(command_name: str, caller: str, is_slash_command: bool = True) -> str:
+    current_time = get_current_time()
+    prefix = "/" if is_slash_command else "!"
+
+    return f"{current_time} | \033[1;33m{prefix}{command_name}\033[0;0m: Called by \033[0;32m{caller}\033[0;0m"
+
+def load_config() -> dict:
+    with open(CONFIG_PATH, "r") as config_file:
+        return json.load(config_file)
+
+def refresh_config(new_configs: dict) -> None:
+    config_file = open(CONFIG_PATH, "w")
+
+    json.dump(new_configs, config_file, indent=4)
